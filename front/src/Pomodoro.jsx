bae31fa40c4e73be7605faff8a2b9222919fae76
Florian <Florian.debosz@outlook.fr>
2023-04-03 21:06:37 +0200

    CSS patch

---
+++ front/src/Pomodoro.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Pomodoro(){
+    return (
+        <div className="pomodoro">
+            <div className="message">
+                <div>Break! New session starts in :</div>
+            </div>
+            <div className="timer">25:00</div>
+        </div>
+    )
+}
