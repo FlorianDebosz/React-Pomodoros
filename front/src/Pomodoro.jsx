cc89c09800590590a8760a427759b60b494064d3
Florian <Florian.debosz@outlook.fr>
2023-04-04 12:37:16 +0200

    Basic Training Pomodoros without session

--- front/src/Pomodoro.jsx
+++ front/src/Pomodoro.jsx
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Pomodoro(){
+    const [minutes,setMinutes] = useState(0);
+    const [seconds,setSeconds] = useState(5);
+    const [displayMessage, setDisplayMessage] = useState(false);
+
+    useEffect(() => {
+        let interval = setInterval(() => {
+            clearInterval(interval);
+            
+            if(seconds === 0) {
+                if(minutes !== 0) {
+                    setSeconds(59);
+                    setMinutes(minutes - 1);
+                }else {
+                    let minutes = displayMessage ? 24 : 4;
+                    let seconds = 59;
+
+                    setMinutes(minutes);
+                    setSeconds(seconds);
+                    setDisplayMessage(!displayMessage);
+                }
+            }else {
+                setSeconds(seconds - 1);
+            }
+        },1000)
+    },[seconds])
+
+    const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
+    const timerSeconds = seconds < 10 ? `0${seconds}` : seconds;
     return (
         <div className="pomodoro">
             <div className="message">
-                <div>Break! New session starts in :</div>
+                {displayMessage &&<div>Break! New session starts in :</div>}
             </div>
-            <div className="timer">25:00</div>
+            <div className="timer">{timerMinutes}:{timerSeconds}</div>
         </div>
     )
 }
